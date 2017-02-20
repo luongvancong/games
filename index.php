02177af8eeeeb2cf5f7394f1f6375b8409adff2f
Justin <cong.itsoft@gmail.com>
2017-02-20 14:37:23 +0700

    games

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Game</title>
+</head>
+<body>
+
+<ul>
+    <li>
+        <a href="/ban_ruoi/">Bắn ruồi</a>
+    </li>
+    <li>
+        <a href="/dude">Dude</a>
+    </li>
+</ul>
+
+</body>
+</html>
